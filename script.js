516e20060b57c80e64526e080ab22a6b798c0509
Ryan Wilkinson <ryanwilkinson@macbook-pro.mynetworksettings.com>
2023-10-27 21:31:16 -0400

    new picture, fixed formatting, added graphics

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function() {
         let deleteInterval = setInterval(() => {
             word = word.substring(0, word.length - 1);
             changingTextElement.textContent = word;
-            
+
             if (word.length === 0) {
                 clearInterval(deleteInterval);
                 currentWord = (currentWord + 1) % wordArray.length;
@@ -40,14 +40,34 @@ document.addEventListener('DOMContentLoaded', function() {
 
     typeWord();
 
-});
-
-document.querySelectorAll(".portfolio-link, .portfolio-btn").forEach(element => {
-    element.addEventListener("click", function(e) {
-        e.preventDefault(); // Prevents the default action for the link, harmless for the button
-        document.getElementById("portfolioSection").scrollIntoView({ 
-            behavior: 'smooth' 
+    document.querySelectorAll(".portfolio-link, .portfolio-btn").forEach(element => {
+        element.addEventListener("click", function(e) {
+            e.preventDefault(); // Prevents the default action for the link, harmless for the button
+            document.getElementById("portfolioSection").scrollIntoView({
+                behavior: 'smooth'
+            });
         });
     });
+
+    // Check if the URL has the fragment "#portfolio"
+    if (window.location.hash === '#portfolio') {
+        // Wait for a short duration to ensure everything has loaded
+        setTimeout(() => {
+            document.getElementById("portfolioSection").scrollIntoView({
+                behavior: 'smooth'
+            });
+        }, 100); // This delay is to ensure the page has fully rendered before we scroll
+    }
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    const handEmojiElement = document.getElementById('hand-emoji');
+
+    // Add the 'waving-hand' class to trigger the animation
+    handEmojiElement.classList.add('waving-hand');
+
+    // Remove the animation class after 5 seconds
+    setTimeout(() => {
+        handEmojiElement.classList.remove('waving-hand');
+    }, 1500); // This is 1.5 seconds, adjust as needed
+});
